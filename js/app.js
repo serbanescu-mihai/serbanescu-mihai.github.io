eb4097e1e55389a2628eefa6adae5ed5fc044fb0
Mihai Serbanescu <102154186+serbanescu-mihai@users.noreply.github.com>
2022-12-21 13:58:08 +0100

    JS year

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+const get = (element) => {
+  const select = document.querySelector(element);
+  if (select) {
+    return select;
+  } else {
+    throw Error("N-ai selectat nimic");
+  }
+};
+
+const copyrightYear = get("#date");
+copyrightYear.textContent = `${new Date().getFullYear()}`;
